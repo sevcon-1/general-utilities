471aae07822f9de287e368a5c0d2d52b24177e06
sevcon-1 <sevcon.nosql@gmail.com>
2017-08-04 14:01:13 +0100

    Added multi entry option example

--- options.groovy
+++ options.groovy
@@ -1,10 +1,19 @@
+import org.apache.commons.cli.Option;
 /**
  * Wrap a script and groovy jars to an executable jar.
  */
 def cli = new CliBuilder()
-cli.h( longOpt: 'help', required: false, 'show usage information' )
-cli.m( longOpt: 'mainclass', argName: 'mainclass', required: false, args: 1, 'fully qualified main class' )
-cli.c( longOpt: 'groovyc', required: false, 'Run groovyc' )
+//cli.h( longOpt: 'help', required: false, 'show usage information' )
+//cli.m( longOpt: 'mainclass', argName: 'mainclass', required: false, args: 1, 'fully qualified main class' )
+//cli.c( longOpt: 'groovyc', required: false, 'Run groovyc' )
+//cli.f( longOpt: 'arguments', args: Option.UNLIMITED_VALUES, required: true, valueSeparator: ',' as char,'Unlimited arguments, separated by a comma')
+
+// Can also populate cli as so:
+cli.with {
+            h longOpt: 'help', required: false, 'show usage information'
+            f longOpt: 'arguments', args: Option.UNLIMITED_VALUES, required: true, valueSeparator: ',' as char,
+                    'Unlimited arguments, separated by a comma'
+        }
 
 //--------------------------------------------------------------------------
 def opt = cli.parse(args)
@@ -14,6 +23,13 @@ if (opt.h) {
   return 
 }
 
+if (opt.f) {
+    fName = opt.getOptionValues("f")
+    fName.each {println it}
+}
+
+
+//Example manipulation and parsing
 //def mainClass = opt.m
 //def scriptBase = mainClass.replace( '.', '/' )
 //def scriptFile = new File( scriptBase + '.groovy' )
